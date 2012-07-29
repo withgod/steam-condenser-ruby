5d5e57ba9fc0d64ae09760202f7f32db288a7995
Sebastian Staudt <koraktor@gmail.com>
2012-07-29 16:27:37 +0200

    Moved DotA2 inventory files into the SteamCondenser namespace

--- lib/steam/community/dota2/dota2_inventory.rb
+++ lib/steam/community/dota2/dota2_inventory.rb
@@ -6,22 +6,26 @@
 require 'steam/community/dota2/dota2_item'
 require 'steam/community/game_inventory'
 
-# Represents the inventory of a DotA 2 player
-#
-# @author Sebastian Staudt
-class Dota2Inventory < GameInventory
+module SteamCondenser
 
-  # Creates a new inventory object for the given SteamID64 in DotA 2
-  # (App ID 570)
+  # Represents the inventory of a DotA 2 player
   #
-  # @param [Fixnum] steam_id64 The 64bit SteamID of the player to get the
-  #        inventory for
-  # @macro cacheable
-  def initialize(steam_id64)
-    super 570, steam_id64
-  end
+  # @author Sebastian Staudt
+  class Dota2Inventory < GameInventory
 
-  # The class representing DotA 2 items
-  @@item_class = Dota2Item
+    # Creates a new inventory object for the given SteamID64 in DotA 2
+    # (App ID 570)
+    #
+    # @param [Fixnum] steam_id64 The 64bit SteamID of the player to get the
+    #        inventory for
+    # @macro cacheable
+    def initialize(steam_id64)
+      super 570, steam_id64
+    end
+
+    # The class representing DotA 2 items
+    @@item_class = Dota2Item
+
+  end
 
 end
